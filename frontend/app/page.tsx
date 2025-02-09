5882cd767e0bedc7a9d50a91d020083a28de91cc
David Smite <davidsmite@pc-269.home>
2025-02-09 20:16:22 +0100

    Mise à jour de la page et ajout du fichier .gitignore

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
-    <div>
-      <h1>Bienvenue sur Smithair Concept</h1>
-      <p>Bienvenue sur notre site.</p>
+    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
+      <h1 className="text-3xl font-bold text-blue-600">Bienvenue sur Smithair Concept</h1>
+      <p className="text-lg text-gray-700">Bienvenue sur notre site.</p>
     </div>
   );
 }
