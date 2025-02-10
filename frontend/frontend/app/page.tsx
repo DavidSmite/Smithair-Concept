c6a2bb20dbee8b38561a24fdee3b84e2fe69f64c
David Smite <davidsmite@pc-269.home>
2025-02-10 02:06:00 +0100

    Correction affichage page principale

--- frontend/frontend/app/page.tsx
+++ frontend/frontend/app/page.tsx
@@ -1,8 +1,18 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
 export default function Home() {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-100">
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <h1 className="text-4xl font-bold text-blue-600">
-        🚀 Smithair Concept est en ligne !
+        🚀 Smithair Concept {isClient ? "est bien chargé !" : "est en cours..."}
       </h1>
     </div>
   );
